b0a6d619cfabcb8af31e6f9960f19223f0de9c60
Edilson <eddylson.londonio@gmail.com>
2021-01-22 21:17:43 -0500

    Gender added

--- Reto1/src/test/mocks/index.js
+++ Reto1/src/test/mocks/index.js
@@ -1,94 +1,144 @@
 const mock1 = {
 
-data: [
+  data: [
 
-	{
-		id_de_caso: '1',
-		edad: "20"
-	},{
-		id_de_caso: '2',
-		edad: "30"
-	},{
-		id_de_caso: '3',
-		edad: "40"
-	},{
-		id_de_caso: '4',
-		edad: "80"
-	},{
-		id_de_caso: '5',
-		edad: "10"
-	},
-	{
-		id_de_caso: '6',
-		edad:'35'
-	}
-]
+    {
+      id_de_caso: '1',
+      edad: '20',
+      sexo: 'F'
+    }, {
+      id_de_caso: '2',
+      edad: '20',
+      sexo: 'M'
+    }, {
+      id_de_caso: '3',
+      edad: '30',
+      sexo: 'F'
+    }, {
+      id_de_caso: '4',
+      edad: '30',
+      sexo: 'M'
+    }, {
+      id_de_caso: '5',
+      edad: '40',
+      sexo: 'F'
+    }, {
+      id_de_caso: '6',
+      edad: '40',
+      sexo: 'M'
+    }, {
+      id_de_caso: '7',
+      edad: '80',
+      sexo: 'F'
+    }, {
+      id_de_caso: '8',
+      edad: '10',
+      sexo: 'M'
+    },
+    {
+      id_de_caso: '9',
+      edad: '35',
+      sexo: 'F'
+    },
+    {
+      id_de_caso: '10',
+      edad: '7',
+      sexo: 'F'
+    }
+  ]
 }
 const mock2 = [
-	{
-		id_de_caso: '1',
-		edad: 20
-	},{
-		id_de_caso: '2',
-		edad: 30
-	},{
-		id_de_caso: '3',
-		edad: 40
-	},{
-		id_de_caso: '4',
-		edad: 80
-	},{
-		id_de_caso: '5',
-		edad: 10
-	},
-	{
-		id_de_caso: '6',
-		edad: 35
-	}
+  {
+    id_de_caso: '1',
+    edad: 20
+  }, {
+    id_de_caso: '2',
+    edad: 30
+  }, {
+    id_de_caso: '3',
+    edad: 40
+  }, {
+    id_de_caso: '4',
+    edad: 80
+  }, {
+    id_de_caso: '5',
+    edad: 10
+  },
+  {
+    id_de_caso: '6',
+    edad: 35
+  }
+
 ]
 
 const mockResponse = {
-	data: {
-		'0_20': [
-			{
-				id_de_caso: '5',
-				edad:'10'
-			}
-		],
-		'20_40':[
+  data: {
+		'0_20_F': [
 		{
-				id_de_caso: '1',
-				edad: '20'
-			},
-			{
-			id_de_caso: '2',
-			edad: '30'
-			},
-			{
-				id_de_caso:'6',
-				edad:'35'
-			},
-			
-		],
-		'40': [
-			{
-				id_de_caso: '3',
-				edad: '40'
-			},
-			{
-				id_de_caso:'4',
-				edad:'80'
-			}
-			
-		]
-	}
+      id_de_caso: '10',
+      edad: '7',
+      sexo: 'F'
+    }
+    ],
+    '0_20_M': [
+      {
+        id_de_caso: '8',
+        edad: '10',
+        sexo: 'M'
+      }
+    ],
+    '20_40_F': [
+      {
+        id_de_caso: '1',
+        edad: '20',
+        sexo: 'F'
+      }, {
+        id_de_caso: '3',
+        edad: '30',
+        sexo: 'F'
+      }, {
+        id_de_caso: '9',
+        edad: '35',
+        sexo: 'F'
+      }
+
+    ],
+    '20_40_M': [
+      {
+        id_de_caso: '2',
+        edad: '20',
+        sexo: 'M'
+      }, {
+        id_de_caso: '4',
+        edad: '30',
+        sexo: 'M'
+      }
+
+    ],
+    '40_F': [
+      {
+        id_de_caso: '5',
+        edad: '40',
+        sexo: 'F'
+      }, {
+        id_de_caso: '7',
+        edad: '80',
+        sexo: 'F'
+      }
+    ],
+    '40_M': [
+      {
+        id_de_caso: '6',
+        edad: '40',
+        sexo: 'M'
+      }
+
+    ]
+  }
 }
 
-
-
-
 module.exports = {
-	mock1,
-	mock2,
-	mockResponse
+  mock1,
+  mock2,
+  mockResponse
 }
--- Reto1/src/utilities/index.js
+++ Reto1/src/utilities/index.js
@@ -2,12 +2,18 @@
 
 function filterInfo (data) {
   const info = data.reduce((previous, current) => {
-    if (!previous['0_20']) previous['0_20'] = []
-    if (!previous['20_40']) previous['20_40'] = []
-    if (!previous['40']) previous['40'] = []
-    if (current.edad >= 0 && current.edad < 20) previous['0_20'].push(current)
-    if (current.edad >= 20 && current.edad < 40) previous['20_40'].push(current)
-    if (current.edad >= 40) previous['40'].push(current)
+    if (!previous['0_20_F']) previous['0_20_F'] = []
+    if (!previous['0_20_M']) previous['0_20_M'] = []
+    if (!previous['20_40_F']) previous['20_40_F'] = []
+    if (!previous['20_40_M']) previous['20_40_M'] = []
+    if (!previous['40_F']) previous['40_F'] = []
+    if (!previous['40_M']) previous['40_M'] = []
+    if (current.edad >= 0 && current.edad < 20 && current.sexo === 'F') previous['0_20_F'].push(current)
+    if (current.edad >= 0 && current.edad < 20 && current.sexo === 'M') previous['0_20_M'].push(current)
+    if (current.edad >= 20 && current.edad < 40 && current.sexo === 'F') previous['20_40_F'].push(current)
+    if (current.edad >= 20 && current.edad < 40 && current.sexo === 'M') previous['20_40_M'].push(current)
+    if (current.edad >= 40 && current.sexo === 'F') previous['40_F'].push(current)
+    if (current.edad >= 40 && current.sexo === 'M') previous['40_M'].push(current)
     return previous
   }, {})
   return info
